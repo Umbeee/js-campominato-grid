c787f7ad70f422bbde0ff1b1c977ba400da12c48
Umbeee <umberto.nesta@hotmail.com>
2023-04-03 12:45:59 +0200

    esercizio completo

---
+++ main.js
@@ -0,0 +1,57 @@
+const btnGioca = document.getElementById('gioca')
+const selectDifficolta = document.getElementById('difficolta')
+const htmlMain = document.querySelector('main')
+const r = document.querySelector(':root')
+
+
+
+btnGioca.addEventListener('click', function(){
+
+    campoMinato();
+})
+
+function campoMinato(){
+
+    let valoreDifficolta = parseInt(selectDifficolta.value);
+
+    myFunction_set(valoreDifficolta);
+    
+
+    htmlMain.innerHTML = '';
+
+
+    let divGriglia = document.createElement('div');
+    divGriglia.classList.add('griglia');
+    htmlMain.append(divGriglia);
+
+
+   
+
+    for(let i= 1; i<=valoreDifficolta; i++){
+
+        let divCella= document.createElement('div');
+        divCella.classList.add('item');
+        divCella.innerText = i;
+
+        document.querySelector('.griglia').append(divCella);
+
+        divCella.addEventListener('click', function(){
+            console.log(this.innerText)
+            
+
+            
+                this.classList.add('clicked')
+           
+                
+        })
+    }
+
+    
+}
+
+function myFunction_set(x) {
+
+    x = Math.sqrt(x);
+    r.style.setProperty('--numCelle', x);
+}
+
